c05247a2c0dc11c4bfb7a814838a6887e928298e
sorrisodps <heittor.aluno@gmail.com>
2023-06-15 23:06:44 -0300

    teste de interface

--- src/SaudeMaisOuMenos/Cliente.java
+++ src/SaudeMaisOuMenos/Cliente.java
@@ -75,4 +75,8 @@ public class Cliente extends Pessoa {
         }
     }
 
+    void venderPlano() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
 }
---
+++ src/SaudeMaisOuMenos/LoginFrame.java
@@ -0,0 +1,70 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package SaudeMaisOuMenos;
+
+import java.awt.FlowLayout;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.JPasswordField;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Thiago
+ */
+public class LoginFrame extends JFrame 
+{
+   private JTextField txtNomePssiente; 
+   private JPasswordField txtCpf; 
+   private JButton btnCadastrar;
+   private JLabel lblnome;
+   private JLabel lblcpf;
+   private JLabel lblidade;
+   private JLabel lbldatanasc;
+   private JLabel lblMsg;
+   
+   public LoginFrame(Pessoa pessoa){
+      super( "Login" );
+      setLayout( new FlowLayout() );
+      
+      //Constroi os componentes
+      btnCadastrar = new JButton("Salvar");
+      txtNomePssiente = new JTextField(10);     
+      txtcpf = new JTextField(10);
+      lblnome = new JLabel("Nome");
+      lblcpf = new JLabel("CPF");
+      lblidade = new JLabel("Idade");
+      lbldatanasc = new JLabel("Data de nascimento");
+      lblMsg = new JLabel("Cliente cadastrado com sucesso");
+      
+      //Adiciona os componentes
+      add(lblnome);
+      add(lblcpf);
+      add(lblidade);
+      add(lbldatanasc);
+      add(txtNomePssiente);
+      add(lblcpf);
+      add(txtCpf );
+      add(btnCadastrar);     
+      add(lblMsg); 
+    
+     btnCadastrar.addActionListener(e -> {
+          cred.setCredenciais(txtNomePssiente.getText(), txtCpf.getText());
+          System.out.println(txtNomePssiente.getText() + txtCpf.getText());
+          this.dispose(); //fecha a caixa de diálogo
+     }); 
+   }
+   
+   public void exibir(){
+        this.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+        this.setSize(400, 100);
+        this.setVisible(true);
+   }
+ 
+}
